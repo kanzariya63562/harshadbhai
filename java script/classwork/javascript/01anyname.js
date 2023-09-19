e69f14994c3a446c3da46908436433c7c33cfe38
kanzariya63562 <harshadbhai27840@gmail.com>
2023-09-19 10:42:19 +0530

    java script
    java script

---
+++ java script/classwork/javascript/01anyname.js
@@ -0,0 +1,16 @@
+
+  // to display CONTENT IN BODY/DOCUMENT/WINDOW
+        
+  document.write("hello JS 123 <u><b>^&%</b></u>");
+  // to display CONTENT in A CONSOLE 
+              console.log('hello JS 123 <u><b>^&%</b></u>');
+  
+              // to display CONTENT in ANY TAG 
+  
+              // document.getElementById('t');
+  
+              console.log(document.getElementById('t'));
+  
+              document.getElementById('t').innerHTML = "this is <u>H1</u> through JS";
+  
+           document.getElementById('t').innerText = "this is <u>H1</u> through JS innerTEXT";
