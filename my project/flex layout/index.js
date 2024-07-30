d077f62cf0f7ec5ef21691be2a8d2cae38820e99
kanzariya63562 <harshadbhai27840@gmail.com>
2024-07-30 15:21:34 +0530

    my project
    my project

--- my project/flex layout/index.js
+++ my project/flex layout/index.js
@@ -1,8 +1,1 @@
-document.getElementById('toggle-content').addEventListener('click', function() {
-    const content = document.getElementById('content');
-    if (content.style.display === 'none') {
-        content.style.display = 'block';
-    } else {
-        content.style.display = 'none';
-    }
-});
+
